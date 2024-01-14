f03ec7f5e1ef2f5324cfdd6899b27fe135e8ef2a
datots <dato_ts@hotmail.com>
2024-01-15 02:39:11 +0400

    Javascript added coffee house updated

--- Menu/index.js
+++ Menu/index.js
@@ -14,8 +14,10 @@ function showTab(tabId) {
 // DATA
 const serverData = [
   {
-    image: "https://ibb.co/SNTBpGR",
+    // image: "https://i.ibb.co/5TDLVwB/irish-coffee.png",
+    
     name: "Irish coffee",
+    url: "./assets/irish-coffee.png",
     description:
       "Fragrant black coffee with Jameson Irish whiskey and whipped milk",
     price: "7.00",
@@ -51,6 +53,7 @@ const serverData = [
   },
 
   {
+    image: "https://i.ibb.co/RBR242c/kahlua-coffee.png",
     name: "Kahlua coffee",
     description:
       "Classic coffee with milk and Kahlua liqueur under a cap of frothed milk",
@@ -87,6 +90,7 @@ const serverData = [
   },
 
   {
+    image: "https://i.ibb.co/KyqGy3V/honey-raf.png",
     name: "Honey raf",
     description: "Espresso with frothed milk, cream and aromatic honey",
     price: "5.50",
@@ -122,6 +126,7 @@ const serverData = [
   },
 
   {
+    image: "https://i.ibb.co/r7cHt52/ice-cappuccino.png",
     name: "Ice cappuccino",
     description: "Cappuccino with soft thick foam in summer version with ice",
     price: "5.00",
@@ -157,6 +162,7 @@ const serverData = [
   },
 
   {
+    image: "https://i.ibb.co/vmjGyTL/espresso.png",
     name: "Espresso",
     description: "Classic black coffee",
     price: "4.50",
@@ -192,6 +198,7 @@ const serverData = [
   },
 
   {
+    image: "https://i.ibb.co/qYxT27S/latte.png",
     name: "Latte",
     description:
       "Espresso coffee with the addition of steamed milk and dense milk foam",
@@ -228,6 +235,7 @@ const serverData = [
   },
 
   {
+    image: "https://i.ibb.co/4WmfqNz/latte-macchiato.png",
     name: "Latte macchiato",
     description: "Espresso with frothed milk and chocolate",
     price: "5.50",
@@ -263,6 +271,7 @@ const serverData = [
   },
 
   {
+    image: "https://i.ibb.co/s64qh00/coffeewithcognac.png",
     name: "Coffee with cognac",
     description: "Fragrant black coffee with cognac and whipped cream",
     price: "6.50",
@@ -298,6 +307,7 @@ const serverData = [
   },
 
   {
+    image: "https://i.ibb.co/s5Ycjqn/tea-1-moroccan.png",
     name: "Moroccan",
     description:
       "Fragrant black tea with the addition of tangerine, cinnamon, honey, lemon and mint",
@@ -334,6 +344,7 @@ const serverData = [
   },
 
   {
+    image: "https://i.ibb.co/Lt2t6tH/tea-2-ginger.png",
     name: "Ginger",
     description: "Original black tea with fresh ginger, lemon and honey",
     price: "5.00",
@@ -369,6 +380,7 @@ const serverData = [
   },
 
   {
+    image: "https://i.ibb.co/3hJ8BKg/tea-3-cranberry.png",
     name: "Cranberry",
     description: "Invigorating black tea with cranberry and honey",
     price: "5.00",
@@ -404,6 +416,7 @@ const serverData = [
   },
 
   {
+    image: "https://i.ibb.co/WVZrX6R/tea-4-sea-buckthorn.png",
     name: "Sea buckthorn",
     description:
       "Toning sweet black tea with sea buckthorn, fresh thyme and cinnamon",
@@ -440,6 +453,7 @@ const serverData = [
   },
 
   {
+    image: "https://i.ibb.co/KLJgsWz/dessert-1-marble-cheesecake.png",
     name: "Marble cheesecake",
     description:
       "Philadelphia cheese with lemon zest on a light sponge cake and red currant jam",
@@ -476,6 +490,7 @@ const serverData = [
   },
 
   {
+    image: "https://i.ibb.co/s540wR5/dessert-2-red-velvet.png",
     name: "Red velvet",
     description: "Layer cake with cream cheese frosting",
     price: "4.00",
@@ -511,6 +526,7 @@ const serverData = [
   },
 
   {
+    image: "https://i.ibb.co/3sgt74m/dessert-3-cheesecakes.png",
     name: "Cheesecakes",
     description:
       "Soft cottage cheese pancakes with sour cream and fresh berries and sprinkled with powdered sugar",
@@ -547,6 +563,7 @@ const serverData = [
   },
 
   {
+    image: "https://i.ibb.co/1bGPCJ4/dessert-4-creme-brulee.png",
     name: "Creme brulee",
     description:
       "Delicate creamy dessert in a caramel basket with wild berries",
@@ -583,6 +600,7 @@ const serverData = [
   },
 
   {
+    image: "https://i.ibb.co/rv80c4z/dessert-5-pancakes.png",
     name: "Pancakes",
     description: "Tender pancakes with strawberry jam and fresh strawberries",
     price: "4.50",
@@ -618,6 +636,7 @@ const serverData = [
   },
 
   {
+    image: "https://i.ibb.co/JktFQxC/dessert-6-honey-cake.png",
     name: "Honey cake",
     description: "Classic honey cake with delicate custard",
     price: "4.50",
@@ -653,6 +672,7 @@ const serverData = [
   },
 
   {
+    image: "https://i.ibb.co/CVfTfBh/dessert-7-chocolate-cake.png",
     name: "Chocolate cake",
     description: "Cake with hot chocolate filling and nuts with dried apricots",
     price: "5.50",
@@ -688,6 +708,7 @@ const serverData = [
   },
 
   {
+    image: "https://i.ibb.co/P58qjcw/dessert-8-black-forest.png",
     name: "Black forest",
     description:
       "A combination of thin sponge cake with cherry jam and light chocolate mousse",
@@ -770,7 +791,17 @@ cards.forEach((item) => {
       serverData[id].description;
     document.getElementById("modalPrice").innerHTML =
       "Price: $" + serverData[id].price;
-    document.getElementById("size").innerHTML = serverData[id].sizes;
-    document.getElementById("additive").innerHTML = serverData[id].additives;
+    document.getElementById("size-1").innerHTML = serverData[id].sizes.s.size;
+    document.getElementById("size-2").innerHTML = serverData[id].sizes.m.size;
+    document.getElementById("size-3").innerHTML = serverData[id].sizes.l.size;
+    document.getElementById("additive-1").innerHTML =
+      serverData[id].additives[0].name;
+    document.getElementById("additive-2").innerHTML =
+      serverData[id].additives[1].name;
+    document.getElementById("additive-3").innerHTML =
+      serverData[id].additives[2].name;
+
+    const modalImg = document.getElementById(".modal-img");
+    modalImg.src = serverData[id].url;
   });
 });
